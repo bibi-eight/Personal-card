d12262c1978af6b3bd3f5b49da76ecde23ec1813
Brenda Brito <bbritob8@gmail.com>
2022-05-21 15:53:52 -0300

    my first commit, upload all files

---
+++ data.js
@@ -0,0 +1,3 @@
+const data ='Brenda Brito, 18 anos, Back-end\n Email: bbritob8@gmail.com\n GitHub: bibi-eight';
+
+export default data;
---
+++ index.js
@@ -0,0 +1,10 @@
+import colors from 'colors';
+import data from './data.js';
+
+const style = colors.setTheme({
+    title: ['rainbow', 'bgWhite']
+});
+
+const text = data.title;
+
+export default text;
