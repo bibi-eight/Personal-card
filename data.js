87c45348df32a31126e10b616dcaef67435a4b2d
brenda <bbritob8@gmail.com>
2023-11-14 14:44:44 -0300

    ajuste no texto

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-const data ='Brenda Brito, 18 anos, Back-end\n Email: bbritob8@gmail.com\n GitHub: bibi-eight';
+const data ='\nBrenda Brito, 18 anos, Back-end\n Email: bbritob8@gmail.com\n GitHub: bibi-eight\n';
 
 export default data;
